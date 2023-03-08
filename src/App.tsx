b642aaad0c74c8c170c2e7097bb246e42446f801
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-03-08 15:19:11 +0100

    restaurant get

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import axios from "axios";
 import "./App.scss";
 
 function App() {
+// Make a request for a user with a given ID
+axios.get('https://school-restaurant-api.azurewebsites.net/restaurant/6408978376187b915f68e168')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
   return (
     <>
       <header className="home">
