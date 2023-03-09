fa61842ad9291b6bdbebf163dfb2db668ef45f08
Sebbe10 <114911492+Sebbe10@users.noreply.github.com>
2023-03-09 16:01:35 +0100

    Search

---
+++ src/components/Admin/searchbookning.tsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+
+import { ISearchthebooking } from "../../models/ISearch";
+import { ISearchBookings } from "../../models/ISearchBookings";
+
+export const Admin = () => {
+  (document.getElementById("searchForm") as HTMLFormElement).addEventListener(
+    "submit",
+    (e: SubmitEvent) => {
+      e.preventDefault();
+
+      let searchText = handleSearchText();
+
+      getData(searchText);
+    }
+  );
+
+  function handleSearchText(): string {
+    let searchInput: HTMLInputElement = document.getElementById(
+      "searchText"
+    ) as HTMLInputElement;
+
+    let searchText: string = searchInput.value;
+    searchInput.value = "";
+
+    return searchText;
+  }
+
+  function getData(searchText: string) {
+    axios
+      .get<ISearchthebooking>(
+        `https://school-restaurant-api.azurewebsites.net/${searchText}api-doc/custimer/:id`
+      )
+      .then((response) => {
+        // createHtml(response.data.Search);
+      });
+  }
+
+  function createHtml(search: ISearchBookings[]) {
+    let container: HTMLDivElement = document.getElementById(
+      "movieContainer"
+    ) as HTMLDivElement;
+
+    container.innerHTML = "";
+    for (let i = 0; i < search.length; i++) {
+      let name1: HTMLHeadingElement = document.createElement("h3");
+
+      //   name1.innerHTML = [i].id;
+
+      container.appendChild(name1);
+    }
+  }
+  return (
+    <>
+      <form id="searchForm">
+        <input type="text" id="searchText" />
+        <button>Sök</button>
+      </form>
+      <div id="movieContainer"></div>
+    </>
+  );
+};
