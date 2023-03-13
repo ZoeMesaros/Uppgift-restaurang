c2b26231a46f5b9a2e5ea3cae50642b7ce1f011a
Sebbe10 <114911492+Sebbe10@users.noreply.github.com>
2023-03-13 13:19:55 +0100

    Search

--- src/components/Admin/Admin.tsx
+++ src/components/Admin/Admin.tsx
@@ -1,11 +1,10 @@
-import { SearchAdmin } from "../../Service/searchbookning";
+// import { SearchAdmin } from "../../Service/searchbookning";
 import { Bookings } from "./Crud/GetBookings";
 
 export const Admin = () => {
   return (
     <>
       <h1>Admin</h1>
-      <SearchAdmin />
       <Bookings />
     </>
   );
--- src/components/Admin/Crud/GetBookings.tsx
+++ src/components/Admin/Crud/GetBookings.tsx
@@ -1,11 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import { IBooking } from "../../../models/IBooking";
-import { getBookings, getCustomers } from "../../../services/bookingService";
+import { getBookings } from "../../../services/bookingService";
 import { Booking } from "./ShowBooking";
-import './GetBookings.scss'
+import "./GetBookings.scss";
 
 export const Bookings = () => {
   const [bookings, setBookings] = useState<IBooking[]>([]);
+
+  //
+  const [searchText, setSearchText] = useState("");
+
+  const handleSubmit = () => {
+    bookings.find((x) => x.customerId === searchText);
+  };
 
   useEffect(() => {
     const getData = async () => {
@@ -20,21 +27,38 @@ export const Bookings = () => {
     return <Booking booking={booking} key={booking._id}></Booking>;
   });
   return (
-    <table className="bookings">
-      <thead>
-        <tr>
-          <th>Bokning Id</th>
-          <th>Kund If</th>
-          <th>Datum</th>
-          <th>Tid</th>
-          <th>Gäster</th>
-          <th>Förnamn</th>
-          <th>Efternamn</th>
-          <th>E-mail</th>
-          <th>Telefon</th>
-        </tr>
-      </thead>
-      <tbody>{bookingsHtml}</tbody>
-    </table>
+    <>
+      <form id="searchForm" onSubmit={handleSubmit}>
+        <p className="searchbookings">Sök bokning</p>
+        <div className="btn-input">
+          <input
+            type="text"
+            id="searchText"
+            value={searchText}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button id="button">Sök</button>
+        </div>
+      </form>
+      <div id="bookingContainer"></div>
+      <table className="bookings">
+        <thead>
+          <tr>
+            <th>Bokning Id</th>
+            <th>Kund If</th>
+            <th>Datum</th>
+            <th>Tid</th>
+            <th>Gäster</th>
+            <th>Förnamn</th>
+            <th>Efternamn</th>
+            <th>E-mail</th>
+            <th>Telefon</th>
+          </tr>
+        </thead>
+        <tbody>{bookingsHtml}</tbody>
+      </table>
+    </>
   );
 };
