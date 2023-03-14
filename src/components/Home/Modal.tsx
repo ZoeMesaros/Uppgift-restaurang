f3e3b89ddb4a6e89ce9a9b1af8b6ee18cc7c281b
Zoë <35260156+ZoeMesaros@users.noreply.github.com>
2023-03-14 13:16:45 +0100

    gdpr

--- src/components/Home/Modal.tsx
+++ src/components/Home/Modal.tsx
@@ -1,6 +1,6 @@
 import "./Modalstyles.scss";
 
-export default function Modal({ open, children, onClose, rememberUser }: any) {
+export default function Modal({ open, onClose }: any) {
   if (!open) return null;
   const handleClose = (event: { preventDefault: () => void }) => {
     onClose();
@@ -10,16 +10,18 @@ export default function Modal({ open, children, onClose, rememberUser }: any) {
     <>
       <div className="overlay" />
       <div className="modalStyle">
+        <h3>Användarvillkor</h3>
         <form onSubmit={handleClose}>
-          {children}
-          <h3>Användarvillkor</h3>
           <p>
-            Vi samlar in användaruppgifter i enlighet med gdpr för att öka
-            användarvänligheten.
+            Vi hanterar personuppgifter enligt GDPR för att <br /> underlätta
+            användarupplevelsen
           </p>
           <input type="checkbox" name="check" required />
           <label className="checkLabel">Jag godkänner användarvillkoren</label>
-          <button onSubmit={handleClose}>Jag samtycker</button>
+          <br />
+          <button className="agreed" onSubmit={handleClose}>
+            Jag samtycker
+          </button>
           <button>Läs mer</button>
         </form>
       </div>
